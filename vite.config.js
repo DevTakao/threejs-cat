85534b9239a712355dea72df1347125c8df1daa0
Taka <taka@visibleone.com>
2023-10-31 09:51:06 +0630

    chore: remove unused config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/threejs-cat/",
   assetsInclude: ["assets/models/*.glb"],
-  //   build: {
-  //     rollupOptions: {
-  //       output: {
-  //         // Enable hashed URL mode
-  //         manualChunks: () => "hashed",
-  //       },
-  //     },
-  //   },
 });
